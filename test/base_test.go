77391bb6680ca855be3b5ea98172d46e5d4781f6
shanyy <shanyy163@163.com>
2018-03-23 20:35:56 +0800

    Add test file(basic)

---
+++ test/base_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+	"github.com/sleagon/health-checker/base"
+)
+
+func TestLoadConfigOk(t *testing.T) {
+	base.LoadConfig("config.ok.json")
+	cfg := base.GetConfig()
+	if cfg.ProjectName != "HealthCheckerOk" {
+		t.Error("Failed to load legal config file.")
+	}
+}
+
+func TestLoadConfigErr(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Panic should be fired when using a broken config file.")
+			return
+		}
+	}()
+	base.LoadConfig("test/config.err.json")
+	cfg := base.GetConfig()
+	if cfg.ProjectName != "HealthCheckerOk" {
+		t.Error("Failed to load legal config file.")
+	}
+}
